13b2ef7d1ede4f503d029b7b84cf5a059dd6ee12
Taitan <110903767+TaitanB@users.noreply.github.com>
2023-07-28 15:24:04 +0300

    fix parseBody

--- middlewares/parseBody.js
+++ middlewares/parseBody.js
@@ -1,15 +1,18 @@
 const parseBody = async (req, res, next) => {
-  try {
-    if (req.body.data && typeof req.body.data === "string") {
-      req.body = JSON.parse(req.body.data);
-    } else {
-      req.body = {};
-    }
-    next();
-  } catch (error) {
-    console.log("parseBody");
-    res.status(400).json({ error: "Invalid JSON data" });
-  }
+  req.body = JSON.parse(req.body);
+  next();
+
+  // try {
+  //   if (req.body.data && typeof req.body.data === "string") {
+  //     req.body = JSON.parse(req.body.data);
+  //   } else {
+  //     req.body = {};
+  //   }
+  //   next();
+  // } catch (error) {
+  //   console.log("parseBody");
+  //   res.status(400).json({ error: "Invalid JSON data" });
+  // }
 };
 
 module.exports = parseBody;
