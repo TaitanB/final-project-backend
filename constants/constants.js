93f3426415d1362c4ac268e757bf5dfc027573fd
Taitan <110903767+TaitanB@users.noreply.github.com>
2023-07-29 21:55:28 +0300

    update Regex

--- constants/constants.js
+++ constants/constants.js
@@ -1,9 +1,10 @@
-const nameRegex = /^[A-Za-z]{2,16}$/;
+const nameRegex = /^[A-Za-zА-Яа-яЁёІіЇїЄєҐґ]{2,16}$/;
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-z.-]+\.[a-z]{2,}$/;
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/;
 const phoneRegex = /^\+380\d{9}$/;
 const birthdayRegex = /^(0[1-9]|[12]\d|3[01])-(0[1-9]|1[0-2])-\d{4}$/;
-const cityRegex = /^[A-Za-z\s]+(?:,\s*[A-Za-z\s]+)*$/;
+const cityRegex =
+  /^[A-Za-zА-Яа-яЁёІіЇїЄєҐґ\s]+(?:,\s*[A-Za-zА-Яа-яЁёІіЇїЄєҐґ\s]+)*$/;
 
 const perPage = 12;
 
